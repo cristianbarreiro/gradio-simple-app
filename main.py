50ec287effd8d1fcd053de29270ae01f6a1e7509
Cristian <122134947+cristianbarreiro@users.noreply.github.com>
2025-07-05 13:25:47 -0300

    feat: image resizer
    reescalar una imágen

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import gradio as gr
 
-def analyze_text(file):
-    text = file.name
-
-    with open(text, 'r', encoding='utf-8') as f:
-        content = f.read()
-    word_count = len(content.split())
-    return f"The file contains {word_count} words."
+def resize_image(image, width, height):
+    resized_image = image.resize((width, height))
+    # resized_image.save("resized_image.png")
+    return resized_image
 
 interface = gr.Interface(
-    fn=analyze_text,
-    inputs= gr.File(label="Upload a file with two numbers (e.g., numbers.txt)"),
-     outputs=gr.Textbox(label="World Count Result")
+    fn=resize_image,
+    inputs= [
+        gr.Image(type="pil", label="Upload an Image"),
+        gr.Number(label="Width(px)"),
+        gr.Number(label="Height(px)")
+    ],
+     outputs=gr.Image(label="World Count Result")
 )
 
 interface.launch()
