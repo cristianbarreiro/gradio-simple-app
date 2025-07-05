054219fca8b4dd7015c1e65bd995242b5e1d9813
Cristian <122134947+cristianbarreiro@users.noreply.github.com>
2025-07-05 00:40:15 -0300

    feat: calculator

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import gradio as gr
 
-def square_number(number):
-    return number ** 2
+def calculator(number1, number2, operation):
+    if operation == "Addition":
+        return number1 + number2
+    elif operation == "Subtraction":
+        return number1 - number2
+    elif operation == "Multiplication":
+        return number1 * number2
+    elif operation == "Division":
+        if number2 != 0:
+            return number1 / number2
+        else:
+            return "Error: Division by zero is not allowed."
 
 interface = gr.Interface(
-    fn=square_number,
-    inputs=gr.Slider(minimum=0, maximum=100, step=1, label="Input Number"),
-     outputs=gr.Number(label="Result (Square of Input Number)"),
+    fn=calculator,
+    inputs=[  
+        gr.Number(label="First Number"),
+        gr.Number(label="Second Number)"),
+        gr.Dropdown(choices=["Addition", 
+                             "Subtraction", 
+                             "Multiplication", 
+                             "Division"], label="Operation")
+        ],
+     outputs=gr.Number(label="Result")
 )
 
 interface.launch()
